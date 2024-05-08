b9723b6fbaba14c85c5569fe26f0615933b9fc37
Eriseld1 <159956782+Eriseld1@users.noreply.github.com>
2024-05-08 18:23:35 -0400

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
 	return (
-		<div className="App">
-			<h1>Weather App</h1>
-			<footer>
-				This project was coded by{" "}
-				<a
-					href="https://github.com/Eriseld1"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Eriselda Portillo
-				</a>{" "}
-				and is{" "}
-				<a
-					href="https://github.com/Eriseld1/react-weather-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					open-sourced on GitHub
-				</a>{" "}
-				and{" "}
-				<a
-					href="https://moonlit-gaufre-6c35d6.netlify.app/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					hosted on Netlify
-				</a>
-			</footer>
+		<div className="App ">
+			<div className="container">
+				<Weather />
+				<footer>
+					This project was coded by{" "}
+					<a
+						href="https://github.com/Eriseld1"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Eriselda Portillo
+					</a>{" "}
+					and is{" "}
+					<a
+						href="https://github.com/Eriseld1/react-weather-app"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						open-sourced on GitHub
+					</a>{" "}
+					and{" "}
+					<a
+						href="https://moonlit-gaufre-6c35d6.netlify.app/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						hosted on Netlify
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
 							type="search"
 							placeholder="Enter a city.."
 							className="form-control"
+							autoFocus="on"
 						/>
 					</div>
 					<div className="col-3">
-						<input type="submit" value="Search" className="btn btn-primary" />
+						<input
+							type="submit"
+							value="Search"
+							className="btn btn-primary w-100"
+						/>
 					</div>
 				</div>
 			</form>
@@ -29,17 +34,17 @@ export default function Weather() {
 						src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
 						alt="Mostly cloudy"
 					></img>
-					6°C
+					<span className="temperature">6</span>
+					<span className="unit">°C</span>
 				</div>
 				<div className="col-6">
 					<ul>
-						<li>Precipitatio: 15%</li>
+						<li>Precipitation: 15%</li>
 						<li>Humidity: 72%</li>
 						<li>Wind: 13 km/m</li>
 					</ul>
 				</div>
 			</div>
-			Hello from weather
 		</div>
 	);
 }
