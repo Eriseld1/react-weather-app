aaa93ac07befce8c36552c69f1b0dd1944685770
Eriseld1 <159956782+Eriseld1@users.noreply.github.com>
2024-05-08 17:39:01 -0400

    Added github repo URL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="App">
+			<h1>Weather App</h1>
+			<footer>
+				This project was coded by Eriselda Portillo and is{" "}
+				<a
+					href="https://github.com/Eriseld1/react-weather-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					open-sourced on GitHub
+				</a>{" "}
+				and{" "}
+				<a
+					href="https://suspicious-beaver-111c4d.netlify.com/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					hosted on Netlify
+				</a>
+			</footer>
+		</div>
+	);
 }
-
-export default App;
