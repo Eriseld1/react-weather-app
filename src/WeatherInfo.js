473dcd7b2b3f2f8a5c6877b9b5b65f9e90aab820
Eriseld1 <159956782+Eriseld1@users.noreply.github.com>
2024-05-22 18:28:13 -0400

    updated forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,6 +19,7 @@ export default function WeatherInfo(props) {
 						<div className="float-left">
 							<WeatherIcon code={props.data.icon} size={52} />
 						</div>
+
 						<div className="float-left">
 							<WeatherTemperature celsius={props.data.temperature} />
 						</div>
@@ -27,7 +28,7 @@ export default function WeatherInfo(props) {
 				<div className="col-6">
 					<ul>
 						<li>Humidity: {props.data.humidity}%</li>
-						<li>Wind: {props.data.wind} km/m</li>
+						<li>Wind: {props.data.wind} km/h</li>
 					</ul>
 				</div>
 			</div>
